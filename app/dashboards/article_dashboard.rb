d02e72be718e427f641bb8d71a019eea0f36f256
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-11-26 12:28:53 -0500

    view and dashboard changes for admin and article features

--- app/dashboards/article_dashboard.rb
+++ app/dashboards/article_dashboard.rb
@@ -4,9 +4,8 @@ class ArticleDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
+  # Each type represents an Administrate::Field object, which determines
+  # how the attribute is displayed on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     user_id: Field::String,
@@ -18,19 +17,19 @@ class ArticleDashboard < Administrate::BaseDashboard
 
   # COLLECTION_ATTRIBUTES
   # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :id,
-    :user,
     :title,
     :content
   ]
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = ATTRIBUTE_TYPES.keys
+  SHOW_PAGE_ATTRIBUTES = [
+    :title,
+    :created_at,
+    :content
+  ]
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
